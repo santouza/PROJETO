09f03b8e24fe57b80f31a5feeae818e6d126d976
Lívia <santouza5@gmail.com>
2020-05-14 20:06:12 -0300

    Modified

--- app/views.py
+++ app/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from django.http import HttpResponse
+from django.views import View
+
+class Home(View):
+
+    def get(self, request, *args, **kwargs):
+        return render(request, 'home.html')
